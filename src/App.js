56efc16da1c04f27421f8cc4ec27a490987e3b58
Joshua Gaucin <joshuagaucin01@gmail.com>
2020-08-12 20:08:18 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
+import geeseLogo from './assets/images/geese.jpg'
+import foxLogo from './assets/images/fox.jpg'
+import duckLogo from './assets/images/ducks.jpg'
+import wolfLogo from './assets/images/wolf.jpg'
 import React from 'react';
-import logo from './logo.svg';
+import Game from './components/game';
 import './App.css';
 
-function App() {
+
+
+function App(props) {
+  const ducks = {
+    name: 'ducks',
+    logoSrc: duckLogo
+
+  }
+  const geese = {
+    name: 'geese',
+    logoSrc: geeseLogo
+
+  }
+  const foxes = {
+    name: 'foxes',
+    logoSrc: foxLogo
+
+  }
+  const wolves = {
+    name: 'wolves',
+    logoSrc: wolfLogo
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={ducks}
+        visitingTeam={geese} />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={wolves}
+        visitingTeam={foxes}
+      />
     </div>
-  );
+  )
 }
+
 
 export default App;
