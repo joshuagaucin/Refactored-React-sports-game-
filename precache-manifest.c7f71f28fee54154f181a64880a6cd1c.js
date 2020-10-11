50bd57cc9e60fac4408f662735f2d4a052fa9b0f
Joshua Gaucin <joshuagaucin01@gmail.com>
2020-10-11 16:43:02 -0500

    Updates

---
+++ precache-manifest.c7f71f28fee54154f181a64880a6cd1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc92b1962d2f7690f8568a2e7fb3b86e",
+    "url": "/Refactored-React-sports-game-/index.html"
+  },
+  {
+    "revision": "c71b2c36d66ceb490960",
+    "url": "/Refactored-React-sports-game-/static/css/main.6e74709c.chunk.css"
+  },
+  {
+    "revision": "3909426b83818951cce0",
+    "url": "/Refactored-React-sports-game-/static/js/2.22496f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Refactored-React-sports-game-/static/js/2.22496f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71b2c36d66ceb490960",
+    "url": "/Refactored-React-sports-game-/static/js/main.3962a059.chunk.js"
+  },
+  {
+    "revision": "7c2b1984c440344d44f2",
+    "url": "/Refactored-React-sports-game-/static/js/runtime-main.1c9ba623.js"
+  },
+  {
+    "revision": "87a0d83720e2942cf66672ce27b1bb94",
+    "url": "/Refactored-React-sports-game-/static/media/Bounce.87a0d837.mp3"
+  },
+  {
+    "revision": "6bc3680966212935503457b345b30c39",
+    "url": "/Refactored-React-sports-game-/static/media/Swish.6bc36809.mp3"
+  },
+  {
+    "revision": "b57431dbc93ebf3eb7bd75e61db0d84d",
+    "url": "/Refactored-React-sports-game-/static/media/ducks.b57431db.jpg"
+  },
+  {
+    "revision": "e31806dd53f6b74bcb296e923464da3a",
+    "url": "/Refactored-React-sports-game-/static/media/fox.e31806dd.jpg"
+  },
+  {
+    "revision": "afb1c94df01d609d208a58b7e0c6008e",
+    "url": "/Refactored-React-sports-game-/static/media/geese.afb1c94d.jpg"
+  },
+  {
+    "revision": "4155ab96899e3afaa9352eb998428cd3",
+    "url": "/Refactored-React-sports-game-/static/media/wolf.4155ab96.jpg"
+  }
+]);
